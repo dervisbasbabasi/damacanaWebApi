be8b7eacf33a2bfd3a3af98d40c7c947c0722d54
dbasbabasi <dervis.basbabasi@ogr.gsu.edu.tr>
2015-12-30 10:52:03 +0200

    30.12.15

---
+++ WebApplication5/Models/CartDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class CartDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
